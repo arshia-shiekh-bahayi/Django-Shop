1ff588fa1862f2474f246a62927b9f7281577d7c
arshia-shiekh-bahayi <arshiasheikhbahayi@gmail.com>
2024-09-15 10:21:53 +0430

    not important

--- core/shop/views.py
+++ core/shop/views.py
@@ -11,6 +11,8 @@ from .models import ProductModel, ProductStatusType
 class ShopProductGridViews(ListView):
     template_name = 'shop/product-grid.html'
     queryset = ProductModel.objects.filter(status=ProductStatusType.publish.value)
+    paginate_by = 9
+    
     def get_context_data(self, **kwargs):
         context =  super().get_context_data(**kwargs)
         context["total_items"] = self.get_queryset().count()
